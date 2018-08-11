e088ee43723f6e44eedf3d994f86a6b6bf630723
Abhishek Duggal <abhishekduggal@Abhisheks-MacBook-Pro.local>
2018-08-10 23:14:49 -0500

    Manager Metrics NavBar Design Complete

--- src/Components/Charts/Header_Chart.js
+++ src/Components/Charts/Header_Chart.js
@@ -1,18 +1,25 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { Button } from "reactstrap";
 
 const Header_Chart = () => {
   return (
     <div>
       <h1>Metrics Dashboard</h1>
       <Link to={`/chart/rejects`}>
-        <button className="Next_Button">Reject Analysis</button>
+        <Button outline color="info" className="Next_Button">
+          Reject Analysis
+        </Button>
       </Link>
       <Link to={`/chart/rejects/cost`}>
-        <button className="Next_Button">Reject Cost Analysis</button>
+        <Button outline color="info" className="Next_Button">
+          Reject Cost Analysis
+        </Button>
       </Link>
       <Link to={`/chart/facility`}>
-        <button className="Next_Button">Facility Analysis</button>
+        <Button outline color="info" className="Next_Button">
+          Facility Analysis
+        </Button>
       </Link>
     </div>
   );
